972779a0de07ad811d71d0dc2250341f3f6a6634
Insoo Kim <insoo223@users.noreply.github.com>
2017-07-17 09:32:01 +0900

    remove button defs

--- LCD Shield/CharLCDDHT11/CharLCDDHT11.ino
+++ LCD Shield/CharLCDDHT11/CharLCDDHT11.ino
@@ -67,19 +67,6 @@ Mark Bramwell, July 2010
 // select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-// define some values used by the panel and buttons
-int lcd_key     = 0;
-int adc_key_in  = 0;
-int BackLightState;
-
-#define btnRIGHT  0
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
-#define btnSELECT 4
-#define btnNONE   5
-#define BackLightPin   10
-
 void setup()
 {
  lcd.begin(16, 2);              // start the library
