f4482a866e49377f875292a112a38ee8164f4518
Insoo Kim <insoo223@users.noreply.github.com>
2017-07-17 09:33:53 +0900

    remove irrelevant comments

--- LCD Shield/CharLCDDHT11/CharLCDDHT11.ino
+++ LCD Shield/CharLCDDHT11/CharLCDDHT11.ino
@@ -57,13 +57,6 @@
 
 dht DHT;
 
-/*******************************************************
-
-This program will test the LCD panel and the buttons
-Mark Bramwell, July 2010
-
-********************************************************/
-
 // select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
